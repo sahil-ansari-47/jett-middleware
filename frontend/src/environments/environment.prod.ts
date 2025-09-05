13764ae64a3d6b4707eefad28aedea8e1caa96db
sahil-ansari-47 <sahilansari28112003@gmail.com>
2025-09-06 01:23:51 +0530

    samesite lax

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'],
-  deployendpoint: process.env['BACKEND_URL'],
+  apiUrl: '',
+  deployendpoint: '',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: process.env['API_URL'], // or use process.env at build time
-  deployendpoint: process.env['BACKEND_URL'],
+  apiUrl: '', // or use process.env at build time
+  deployendpoint: '',
 };
